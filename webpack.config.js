2c3439fd3db517626f7121e5d883acda9f3e8fa7
oscar <oscarchavez121@live.com>
2017-04-29 23:22:15 -0500

    added files, added bootstrap to inde.html header

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry: './main.js',
+	output: {
+		filename: './bundle.js'
+	},
+	watch: true,
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: 'style-loader!css-loader'
+			}
+		]
+	}
+};
